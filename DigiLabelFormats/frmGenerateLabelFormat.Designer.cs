5d17ee45c580c243fce3a12b847302ac04382a6c
DigiBelgium <timquirynen@gmail.com>
2016-08-12 15:10:32 +0200

    commit from work

--- DigiLabelFormats/frmGenerateLabelFormat.Designer.cs
+++ DigiLabelFormats/frmGenerateLabelFormat.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
             this.btnOK = new System.Windows.Forms.Button();
             this.txtLabelFormatCode = new System.Windows.Forms.TextBox();
             this.cmbTargetFormat = new System.Windows.Forms.ComboBox();
@@ -73,20 +72,12 @@
             this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(255, 8);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
-            this.label2.TabIndex = 2;
-            // 
             // btnOK
             // 
             this.btnOK.BackColor = System.Drawing.Color.LightGreen;
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnOK.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOK.Location = new System.Drawing.Point(308, 480);
+            this.btnOK.Location = new System.Drawing.Point(292, 480);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(170, 50);
             this.btnOK.TabIndex = 5;
@@ -98,22 +89,22 @@
             this.txtLabelFormatCode.Enabled = false;
             this.txtLabelFormatCode.Location = new System.Drawing.Point(6, 14);
             this.txtLabelFormatCode.Name = "txtLabelFormatCode";
-            this.txtLabelFormatCode.Size = new System.Drawing.Size(493, 22);
+            this.txtLabelFormatCode.Size = new System.Drawing.Size(521, 22);
             this.txtLabelFormatCode.TabIndex = 6;
             // 
             // cmbTargetFormat
             // 
             this.cmbTargetFormat.FormattingEnabled = true;
-            this.cmbTargetFormat.Location = new System.Drawing.Point(20, 16);
+            this.cmbTargetFormat.Location = new System.Drawing.Point(9, 16);
             this.cmbTargetFormat.Name = "cmbTargetFormat";
-            this.cmbTargetFormat.Size = new System.Drawing.Size(99, 22);
+            this.cmbTargetFormat.Size = new System.Drawing.Size(88, 22);
             this.cmbTargetFormat.TabIndex = 8;
             this.cmbTargetFormat.SelectedIndexChanged += new System.EventHandler(this.cmbTargetFormat_SelectedIndexChanged);
             // 
             // pctWeight
             // 
             this.pctWeight.BackColor = System.Drawing.SystemColors.Control;
-            this.pctWeight.Location = new System.Drawing.Point(664, 16);
+            this.pctWeight.Location = new System.Drawing.Point(667, 16);
             this.pctWeight.Name = "pctWeight";
             this.pctWeight.Size = new System.Drawing.Size(336, 483);
             this.pctWeight.TabIndex = 10;
@@ -123,7 +114,7 @@
             // pctPieces
             // 
             this.pctPieces.BackColor = System.Drawing.SystemColors.Control;
-            this.pctPieces.Location = new System.Drawing.Point(1006, 16);
+            this.pctPieces.Location = new System.Drawing.Point(1009, 16);
             this.pctPieces.Name = "pctPieces";
             this.pctPieces.Size = new System.Drawing.Size(363, 483);
             this.pctPieces.TabIndex = 11;
@@ -135,7 +126,7 @@
             this.groupBox2.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(14, 120);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(135, 285);
+            this.groupBox2.Size = new System.Drawing.Size(107, 285);
             this.groupBox2.TabIndex = 19;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Select size";
@@ -146,7 +137,7 @@
             this.lstAfmetingen.ItemHeight = 14;
             this.lstAfmetingen.Location = new System.Drawing.Point(6, 19);
             this.lstAfmetingen.Name = "lstAfmetingen";
-            this.lstAfmetingen.Size = new System.Drawing.Size(123, 256);
+            this.lstAfmetingen.Size = new System.Drawing.Size(90, 256);
             this.lstAfmetingen.TabIndex = 0;
             this.lstAfmetingen.SelectedIndexChanged += new System.EventHandler(this.ChangeFormatCode);
             this.lstAfmetingen.DoubleClick += new System.EventHandler(this.btnBrowse_Click);
@@ -156,7 +147,7 @@
             this.btnCancel.BackColor = System.Drawing.Color.LightSalmon;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(153, 480);
+            this.btnCancel.Location = new System.Drawing.Point(127, 480);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(137, 50);
             this.btnCancel.TabIndex = 20;
@@ -169,7 +160,7 @@
             this.groupBox3.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox3.Location = new System.Drawing.Point(13, 430);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(135, 44);
+            this.groupBox3.Size = new System.Drawing.Size(108, 44);
             this.groupBox3.TabIndex = 21;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Select target format";
@@ -178,9 +169,9 @@
             // 
             this.groupBox4.Controls.Add(this.txtLabelFormatCode);
             this.groupBox4.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox4.Location = new System.Drawing.Point(153, 430);
+            this.groupBox4.Location = new System.Drawing.Point(125, 430);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(505, 44);
+            this.groupBox4.Size = new System.Drawing.Size(533, 44);
             this.groupBox4.TabIndex = 22;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Format Code";
@@ -191,7 +182,7 @@
             this.btnBrowse.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBrowse.Location = new System.Drawing.Point(12, 480);
             this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(137, 50);
+            this.btnBrowse.Size = new System.Drawing.Size(109, 50);
             this.btnBrowse.TabIndex = 23;
             this.btnBrowse.Text = "Select from list";
             this.btnBrowse.UseVisualStyleBackColor = false;
@@ -203,7 +194,7 @@
             this.groupBox5.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox5.Location = new System.Drawing.Point(13, 8);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(135, 106);
+            this.groupBox5.Size = new System.Drawing.Size(108, 106);
             this.groupBox5.TabIndex = 24;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Select device";
@@ -214,7 +205,7 @@
             this.lstTypeToestel.ItemHeight = 14;
             this.lstTypeToestel.Location = new System.Drawing.Point(6, 21);
             this.lstTypeToestel.Name = "lstTypeToestel";
-            this.lstTypeToestel.Size = new System.Drawing.Size(123, 74);
+            this.lstTypeToestel.Size = new System.Drawing.Size(91, 74);
             this.lstTypeToestel.TabIndex = 0;
             this.lstTypeToestel.SelectedIndexChanged += new System.EventHandler(this.ChangeDevice);
             // 
@@ -234,7 +225,7 @@
             // 
             this.lblError.AutoSize = true;
             this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblError.Location = new System.Drawing.Point(592, 16);
+            this.lblError.Location = new System.Drawing.Point(664, 16);
             this.lblError.Name = "lblError";
             this.lblError.Size = new System.Drawing.Size(22, 31);
             this.lblError.TabIndex = 32;
@@ -244,9 +235,9 @@
             // 
             this.groupBox11.Controls.Add(this.lstBasicFields);
             this.groupBox11.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox11.Location = new System.Drawing.Point(155, 8);
+            this.groupBox11.Location = new System.Drawing.Point(127, 8);
             this.groupBox11.Name = "groupBox11";
-            this.groupBox11.Size = new System.Drawing.Size(250, 123);
+            this.groupBox11.Size = new System.Drawing.Size(265, 123);
             this.groupBox11.TabIndex = 25;
             this.groupBox11.TabStop = false;
             this.groupBox11.Text = "Basic fields";
@@ -258,7 +249,7 @@
             this.lstBasicFields.Location = new System.Drawing.Point(6, 17);
             this.lstBasicFields.Name = "lstBasicFields";
             this.lstBasicFields.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.lstBasicFields.Size = new System.Drawing.Size(238, 102);
+            this.lstBasicFields.Size = new System.Drawing.Size(253, 102);
             this.lstBasicFields.TabIndex = 0;
             this.lstBasicFields.SelectedIndexChanged += new System.EventHandler(this.ChangeFormatCode);
             // 
@@ -266,9 +257,9 @@
             // 
             this.groupBox12.Controls.Add(this.lstExtendedFields);
             this.groupBox12.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox12.Location = new System.Drawing.Point(408, 8);
+            this.groupBox12.Location = new System.Drawing.Point(398, 8);
             this.groupBox12.Name = "groupBox12";
-            this.groupBox12.Size = new System.Drawing.Size(250, 141);
+            this.groupBox12.Size = new System.Drawing.Size(260, 141);
             this.groupBox12.TabIndex = 26;
             this.groupBox12.TabStop = false;
             this.groupBox12.Text = "Extended fields";
@@ -280,7 +271,7 @@
             this.lstExtendedFields.Location = new System.Drawing.Point(6, 17);
             this.lstExtendedFields.Name = "lstExtendedFields";
             this.lstExtendedFields.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.lstExtendedFields.Size = new System.Drawing.Size(238, 116);
+            this.lstExtendedFields.Size = new System.Drawing.Size(248, 116);
             this.lstExtendedFields.TabIndex = 0;
             this.lstExtendedFields.SelectedIndexChanged += new System.EventHandler(this.ChangeFormatCode);
             // 
@@ -288,9 +279,9 @@
             // 
             this.Traceability.Controls.Add(this.lstTraceability);
             this.Traceability.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Traceability.Location = new System.Drawing.Point(155, 137);
+            this.Traceability.Location = new System.Drawing.Point(127, 137);
             this.Traceability.Name = "Traceability";
-            this.Traceability.Size = new System.Drawing.Size(250, 118);
+            this.Traceability.Size = new System.Drawing.Size(265, 118);
             this.Traceability.TabIndex = 27;
             this.Traceability.TabStop = false;
             this.Traceability.Text = "Traceability";
@@ -302,7 +293,7 @@
             this.lstTraceability.Location = new System.Drawing.Point(6, 21);
             this.lstTraceability.Name = "lstTraceability";
             this.lstTraceability.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.lstTraceability.Size = new System.Drawing.Size(238, 88);
+            this.lstTraceability.Size = new System.Drawing.Size(253, 88);
             this.lstTraceability.TabIndex = 0;
             this.lstTraceability.SelectedIndexChanged += new System.EventHandler(this.ChangeFormatCode);
             // 
@@ -310,9 +301,9 @@
             // 
             this.groupBox1.Controls.Add(this.lstShoplogos);
             this.groupBox1.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(155, 261);
+            this.groupBox1.Location = new System.Drawing.Point(125, 261);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(252, 83);
+            this.groupBox1.Size = new System.Drawing.Size(267, 83);
             this.groupBox1.TabIndex = 26;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Shop logos";
@@ -324,7 +315,7 @@
             this.lstShoplogos.Location = new System.Drawing.Point(6, 21);
             this.lstShoplogos.Name = "lstShoplogos";
             this.lstShoplogos.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.lstShoplogos.Size = new System.Drawing.Size(238, 46);
+            this.lstShoplogos.Size = new System.Drawing.Size(255, 46);
             this.lstShoplogos.TabIndex = 0;
             this.lstShoplogos.SelectedIndexChanged += new System.EventHandler(this.ChangeFormatCode);
             // 
@@ -332,9 +323,9 @@
             // 
             this.grpTemperatures.Controls.Add(this.lstTemperatures);
             this.grpTemperatures.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grpTemperatures.Location = new System.Drawing.Point(408, 155);
+            this.grpTemperatures.Location = new System.Drawing.Point(398, 152);
             this.grpTemperatures.Name = "grpTemperatures";
-            this.grpTemperatures.Size = new System.Drawing.Size(250, 110);
+            this.grpTemperatures.Size = new System.Drawing.Size(260, 110);
             this.grpTemperatures.TabIndex = 26;
             this.grpTemperatures.TabStop = false;
             this.grpTemperatures.Text = "Temperatures";
@@ -345,7 +336,7 @@
             this.lstTemperatures.ItemHeight = 14;
             this.lstTemperatures.Location = new System.Drawing.Point(6, 21);
             this.lstTemperatures.Name = "lstTemperatures";
-            this.lstTemperatures.Size = new System.Drawing.Size(238, 74);
+            this.lstTemperatures.Size = new System.Drawing.Size(248, 74);
             this.lstTemperatures.TabIndex = 0;
             this.lstTemperatures.SelectedIndexChanged += new System.EventHandler(this.ChangeFormatCode);
             // 
@@ -353,9 +344,9 @@
             // 
             this.groupBox9.Controls.Add(this.lstTextFields);
             this.groupBox9.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox9.Location = new System.Drawing.Point(408, 268);
+            this.groupBox9.Location = new System.Drawing.Point(398, 268);
             this.groupBox9.Name = "groupBox9";
-            this.groupBox9.Size = new System.Drawing.Size(250, 142);
+            this.groupBox9.Size = new System.Drawing.Size(260, 142);
             this.groupBox9.TabIndex = 27;
             this.groupBox9.TabStop = false;
             this.groupBox9.Text = "Text fields";
@@ -367,7 +358,7 @@
             this.lstTextFields.Location = new System.Drawing.Point(6, 21);
             this.lstTextFields.Name = "lstTextFields";
             this.lstTextFields.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.lstTextFields.Size = new System.Drawing.Size(238, 102);
+            this.lstTextFields.Size = new System.Drawing.Size(248, 102);
             this.lstTextFields.TabIndex = 0;
             this.lstTextFields.SelectedIndexChanged += new System.EventHandler(this.ChangeFormatCode);
             // 
@@ -375,9 +366,9 @@
             // 
             this.groupBox6.Controls.Add(this.lstInfotag);
             this.groupBox6.Font = new System.Drawing.Font("Cambria", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox6.Location = new System.Drawing.Point(155, 350);
+            this.groupBox6.Location = new System.Drawing.Point(123, 350);
             this.groupBox6.Name = "groupBox6";
-            this.groupBox6.Size = new System.Drawing.Size(254, 78);
+            this.groupBox6.Size = new System.Drawing.Size(269, 78);
             this.groupBox6.TabIndex = 33;
             this.groupBox6.TabStop = false;
             this.groupBox6.Text = "Infotag fields";
@@ -389,7 +380,7 @@
             this.lstInfotag.Location = new System.Drawing.Point(6, 21);
             this.lstInfotag.Name = "lstInfotag";
             this.lstInfotag.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.lstInfotag.Size = new System.Drawing.Size(240, 46);
+            this.lstInfotag.Size = new System.Drawing.Size(257, 46);
             this.lstInfotag.TabIndex = 0;
             this.lstInfotag.SelectedIndexChanged += new System.EventHandler(this.ChangeFormatCode);
             // 
@@ -416,7 +407,6 @@
             this.Controls.Add(this.pctPieces);
             this.Controls.Add(this.pctWeight);
             this.Controls.Add(this.btnOK);
-            this.Controls.Add(this.label2);
             this.Name = "FrmGenerateLabelFormat";
             this.Text = "Select a labelformat";
             this.Load += new System.EventHandler(this.frmGenerateLabelFormat_Load);
@@ -441,7 +431,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.TextBox txtLabelFormatCode;
         private System.Windows.Forms.ComboBox cmbTargetFormat;
